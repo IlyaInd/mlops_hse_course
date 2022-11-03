2458df059e829890042fddff81b4550ee8c5b269
Ilya Indyk <47114236+IlyaInd@users.noreply.github.com>
2022-11-03 19:11:00 +0600

    Add list of models, not 2 fixed ones

--- app.py
+++ app.py
@@ -10,15 +10,27 @@ from sklearn.exceptions import NotFittedError
 app = Flask(__name__)
 api = Api(app, title='API for ML models fit and predict')
 
-models = {1: Ridge(), 2: RandomForestRegressor()}
-dataset = {}
 
+models = [Ridge(), RandomForestRegressor()]
 
+model_fields = api.model('Model', {'model_type': fields.String(enum=['ridge', 'random_forest'], validate=True)})
 @api.route("/models")
 class ModelList(Resource):
     def get(self):
-        """Return all available models"""
-        return [{"model_id": model_id, "model": str(model)} for model_id, model in models.items()]
+        """Return list of all current models"""
+        return [{"model_id": model_id, "model": str(model)} for model_id, model in enumerate(models)]
+
+    @api.expect(model_fields)
+    def post(self):
+        """Add new untrained model"""
+        model_type = api.payload['model_type']
+        if model_type == 'ridge':
+            models.append(Ridge())
+        elif model_type == 'random_forest':
+            models.append(RandomForestRegressor())
+        else:
+            return 'Incorrect type of model, please choose one of these types: [ridge, random_forest]', 400
+        return f'Added new model with model_id={len(models)-1}', 200
 
 
 params_fields = api.model('Hyperparameters', {'params': fields.Raw(default={"alpha": 1.0},
@@ -38,39 +50,37 @@ class Model(Resource):
         return {'model': str(models[model_id]), 'params': models[model_id].get_params()}
 
     def delete(self, model_id):
-        """Delete exist model and create the new one, ie reset it"""
-        if model_id == 1:
-            models[model_id] = Ridge()
-            return "Model successfully reset", 200
-        elif model_id == 2:
-            models[model_id] = RandomForestRegressor()
-            return "Model successfully reset", 200
+        """Drop the model"""
+        if model_id <= len(models) - 1:
+            m = models[model_id].__repr__()
+            del models[model_id]
+            return f"Model {m} was successfully deleted", 200
         else:
-            return "Incorrect model type specified", 404
+            return f"Incorrect model_id specified, must be in range [0, {len(models)-1}]", 404
 
 
-fit_fields = api.model('Train data', {'X': fields.List(fields.Float(), default=[[1], [2], [5]]),
-                                'y': fields.List(fields.Float(), default=[1, 3, 8])})
+fit_fields = api.model('Train data', {'train_data': fields.List(fields.Float(), default=[[1], [2], [5]]),
+                                'target': fields.List(fields.Float(), default=[1, 3, 8])})
 @api.route("/models/<int:model_id>/fit")
 class Fit(Resource):
     @api.expect(fit_fields)
     def put(self, model_id):
         """Fit model with data passed in JSON format"""
-        X, y = np.array(api.payload["X"]), np.array(api.payload["y"])
-        models[model_id].fit(X, y)
-        rmse = mean_squared_error(y, models[model_id].predict(X)) ** 0.5
+        train_data, target = np.array(api.payload["train_data"]), np.array(api.payload["target"])
+        models[model_id].fit(train_data, target)
+        rmse = mean_squared_error(target, models[model_id].predict(train_data)) ** 0.5
         return {"status": "Fitted on train data", "RMSE on train": round(rmse, 4)}
 
 
-predict_fields = api.model('Predict data', {'X': fields.List(fields.Float(), default=[[1], [2], [5]])})
+predict_fields = api.model('Predict data', {'data': fields.List(fields.Float(), default=[[1], [2], [5]])})
 @api.route("/models/<int:model_id>/predict")
 class Predict(Resource):
     @api.expect(predict_fields)
     def put(self, model_id):
         """Return prediction by data passed in JSON format"""
-        X = np.array(api.payload["X"])
+        data = np.array(api.payload["data"])
         try:
-            y_pred = models[model_id].predict(X)
+            y_pred = models[model_id].predict(data)
             return {"y_pred": list(y_pred)}
         except NotFittedError as e:
             return repr(e)
